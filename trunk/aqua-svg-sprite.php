2c914b73fe1d4daf478e11b523ce44b4aba6e293
Tomas Mulder <tcmulder@gmail.com>
2024-08-05 16:06:30 -0600

    Update versions

--- trunk/aqua-svg-sprite.php
+++ trunk/aqua-svg-sprite.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Aqua SVG Sprite
 Description: Create SVG sprites within WordPress.
-Version:     3.0.12
+Version:     3.0.13
 Author:      Tomas Mulder
 Author URI:  http://www.thinkaquamarine.com
 License:     GPL2
